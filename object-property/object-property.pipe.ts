165222675554d0fa41d2d7f9eb5fad6571254e92
cmbonin <bonin.c.m@gmail.com>
2016-08-25 14:15:32 +1000

    equals equals equals

--- object-property/object-property.pipe.ts
+++ object-property/object-property.pipe.ts
@@ -18,8 +18,8 @@ export class ObjectProperty implements PipeTransform {
 	}
 
 	get(obj, key) {
-			return key.split(".").reduce(function(o, x) {
-					return (typeof o == "undefined" || o === null) ? o : o[x];
+			return key.split('.').reduce(function(o, x) {
+					return (typeof o === 'undefined' || o === null) ? o : o[x];
 			}, obj);
 	}
 }
