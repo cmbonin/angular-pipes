ae924474794d3ed4f93e6592234851f829da95c2
cmbonin <bonin.c.m@gmail.com>
2016-08-25 12:54:58 +1000

    New pipe to extract nested properties from objects

---
+++ object-property/object-property.pipe.ts
@@ -0,0 +1,25 @@
+/**
+ * Extracts the value from an object
+ *
+ * Avoids angular errors when object property doesn't exist
+ * 
+ */
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'objectProperty'
+})
+
+export class ObjectProperty implements PipeTransform {
+
+	transform(value, params) {
+		const prop = this.get(value, params);
+		return prop;
+	}
+
+	get(obj, key) {
+			return key.split(".").reduce(function(o, x) {
+					return (typeof o == "undefined" || o === null) ? o : o[x];
+			}, obj);
+	}
+}
